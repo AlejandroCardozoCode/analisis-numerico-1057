52a273cf2ef6d1a2b7d90d0b9314725994157f27
Alejandro <d.alejandro198@gmail.com>
2021-08-09 22:36:15 -0500

    newton full

--- Talleres/Taller 1/raizNewton.py
+++ Talleres/Taller 1/raizNewton.py
@@ -1,18 +1,18 @@
 import sympy as sy 
 from sympy import *
 import math
-
+from mpmath import mp, mpf
 # implementacion de metodo de Newton
 def metodoNewton(v, e, funcion):
     
-    r = float(v - (valorFuncion(funcion,v)/derivada(funcion,v)))
-    iteracions = 0
-    while abs(float(r - v)) > e:
-        i = i + 1
+    r = mpf(v - (valorFuncion(funcion,v)/derivada(funcion,v)))
+    iteraciones = 0
+    while abs(mpf(r - v)) > e:
+        iteraciones = iteraciones + 1
         v = r
-        r = float(v - (valorFuncion(funcion,v)/derivada(funcion,v)))
-    print("El numeor de iteraciones fue de: " + str(iteraciones))
-    return r
+        r = mpf(v - (valorFuncion(funcion,v)/derivada(funcion,v)))
+    print("El numero de iteraciones fue de: " + str(iteraciones))
+    return mpf(r)
     
 # funcioni que valcula el valor de la funcion
 def valorFuncion(funcion,v):
@@ -27,12 +27,17 @@ def derivada(funcion, v):
 x = sy.Symbol('x')
 funcion1 = (cos(x))**2 - x ** 2
 funcion3 =  x ** 3 - 2*(x**2) + ((4/3)*x) - 8/27 
+mp.dps = 0
 
 #impresion del menu de consola
-tolerancia = input("Ingrese el valor de la toleracia: ")
 valor = input("Ingrese el valor: ")
+print("---------------------------------------")
 print("1) (cos(x))**2 - x ** 2")
+print ("2) x sin(x) - 1 en [-1,2]")
 print("3) x ** 3 - 2*(x**2) + ((4/3)*x) - 8/27")
+print ("4) ")
+print ("5) x^3 - 2x - 5 = 0")
+print("---------------------------------------")
 numeroF = int(input("Ingrese la funcion que desea trabajar: "))
 
 #asignacion del valor de la funcion 
@@ -42,5 +47,31 @@ if numeroF == 1:
 elif numeroF == 3:
     funcion = funcion3
 
+toleranciaN = None 
+
+#asignacion de la tolerancia
+print("---------------------------------------")
+print("1) 10^-8")
+print("2) 10^-16")
+print("3) 10^-32")
+print("4) 10^-56")
+print("---------------------------------------")
+valorTolerancia = int(input("Ingrese el valor de la tolerancia deseada: "))
+
+if valorTolerancia == 1:
+    toleranciaN = 1.0e-8 
+    mp.dps = 8 
+elif valorTolerancia == 2:
+    toleranciaN = 1.0e-16  
+    mp.dps = 16 
+elif valorTolerancia == 3:
+    toleranciaN = 1.0e-32  
+    mp.dps = 32 
+elif valorTolerancia == 4:
+    toleranciaN = 1.0e-56  
+    mp.dps = 56
+else:
+    print("Valor de tolerancia no valido")
+
 # ejecucion del codigo 
-print(metodoNewton(float(valor), float(tolerancia), funcion))
+print(mpf(metodoNewton(mpf(valor), mpf(toleranciaN), funcion)))
